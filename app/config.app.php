59304376aeeba05a090a883c78cf6455110e7214
gjerokrsteski <gjero@krsteski.de>
2013-12-20 15:07:06 +0100

    refactoring to routeable api

--- app/config.app.php
+++ app/config.app.php
@@ -47,10 +47,13 @@ $config_app = array(
   |------------------------------------------------------------------------
   */
   'app' => array(
-    'name' => 'MyFirstBlog',
-    'key' => 'some5secret5key5here',
-    'default_controller' => 'blog', //the name of the fallback controller
-	  'routeable' => true
+    'name'               => 'MyFirstBlog',
+    'key'                => 'some5secret5key5here', // application key
+    'default_controller' => 'blog', // the name of the fallback controller
+	  'routeable'          => true, // get cleaner URLs or not
+    'url'                => 'http://localhost/', // URL used to access your application without a trailing slash.
+    'index'              => '', // if you are using mod_rewrite to get cleaner URLs let it empty otherwise set index.php
+    'asset_url'          => '', // the base URL used for your application's asset files
   ),
 
   /*
