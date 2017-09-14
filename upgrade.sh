5c297f1a976a4357b2ba44285242d6bc70599f1b
Gjero <gjero@dimedis.de>
2017-09-14 15:40:40 +0200

    upgrade to v1.11.0

---
+++ upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd pimf-framework/ && \
+git fetch --tags && \
+git checkout v1.11.0 && \
+cd .. && \
+git add pimf-framework/ && \
+git commit -m "moved submodule to v1.11.0" && \
+git push
+
